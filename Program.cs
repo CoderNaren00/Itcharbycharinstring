11acaa816e4a4c2eddc6a002583b6790c33fed38
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-08-13 16:52:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itcharbycharinstring
+{
+    internal class Program
+    {
+        //Declaration & Initialization of String
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine(); //Declaring & intialization of string
+            Console.WriteLine("This is string demo :" + name);
+            Console.ReadKey();
+        }
+    }
+}
